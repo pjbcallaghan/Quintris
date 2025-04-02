bf03d1bc060df58959c62aee6d281c033984165c
Patrick <pjbcallaghan@gmail.com>
2025-04-02 11:31:32 +0100

    Dynamically load leaderboard

--- scripts/ui.js
+++ scripts/ui.js
@@ -20,6 +20,11 @@ export class UI {
     this.quintrisButton = document.querySelector(".js-quintris-button");
     this.hextrisButton = document.querySelector(".js-hextris-button");
     this.gameMode = [...classic, ...quintris];
+
+    this.viewLeaderboardsButton = document.querySelector(".js-view-lb-button")
+    this.leaderboard = document.querySelector(".leaderboard");
+    this.leaderboardEntries = document.querySelector(".leaderboard-entries")
+    this.leaderboardContents = [];
   }
 
   updateScore(score) {
@@ -67,35 +72,75 @@ export class UI {
   bindEvents() {
     this.selectModeButton.addEventListener('click', () => this.showSelectMode());
     this.classicButton.addEventListener('click', () => this.changeGameMode(classic));
-    this.quintrisButton.addEventListener('click',  () => this.changeGameMode(quintris));
+    this.quintrisButton.addEventListener('click', () => this.changeGameMode(quintris));
     this.hextrisButton.addEventListener('click', () => this.changeGameMode(hextris));
-    this.submitScoreButton.addEventListener('click', () => this.submitScore())
+    this.submitScoreButton.addEventListener('click', () => this.submitScore());
+    this.viewLeaderboardsButton.addEventListener('click', () => this.viewLeaderboard());
   }
+
+  async viewLeaderboard() {
+    await this.fetchLeaderboard(); 
+
+    let htmlContent = "";
+    for (let i = 0; i < 8; i++) {
+        if (this.leaderboardContents[i] !== undefined) {
+            htmlContent += `
+            <div class="leaderboard-entry">
+                <div class="user-rank">
+                  ${i + 1}
+                </div>
+                <div class="user-name">
+                  ${this.leaderboardContents[i].name} 
+                </div>
+                <div class="user-score">
+                  ${this.leaderboardContents[i].score} 
+                </div>
+            </div>
+            `;
+        } else {
+            break;
+        }
+    }
+
+    this.leaderboardEntries.innerHTML = htmlContent;
+    this.menu.style.display = "none";
+    this.leaderboard.style.display = "flex";
+}
 
   bindStartGame(callback) {
     this.startGameButton.addEventListener('click', callback);
     this.playAgain.addEventListener('click', callback);
   }
 
+
+  async fetchLeaderboard() {
+    const res = await fetch("http://localhost:3000/leaderboard");
+    const scores = await res.json();
+    scores.forEach((entry) => {
+      this.leaderboardContents.push(entry)
+    });
+    console.log(this.leaderboardContents)
+  }
+
   async submitScore() {
     if (this.scoreSubmitted === true) { return };
     this.scoreSubmitted = true;
     this.submitScoreButton.innerHTML = 'Score submitted'
-    
+
     const name = document.querySelector(".ss-input").value;
     const score = this.score;
-  
+
     const response = await fetch("http://localhost:3000/leaderboard", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ name, score })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, score })
     });
-  
+
     const data = await response.json();
     console.log("Response:", data);
-    
+
   }
 
 }
